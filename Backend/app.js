54bf5620c2d5feaff874d0f3d986ad0b75998071
Qurat-ul-ain <qurat1356@gmail.com>
2024-05-17 03:45:41 +0500

    init 2

--- Backend/app.js
+++ Backend/app.js
@@ -1,9 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
 require('dotenv').config()
+
 
 const PORT = process.env.PORT
 
+//middleware
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+})
 const server = () => {
-console.log("Tracking 1.0 on port:", PORT);
+    app.listen(PORT, ()=> {
+        console.log("Tracking 1.0 on port:", PORT);
+    }) 
+
 
 }
 
